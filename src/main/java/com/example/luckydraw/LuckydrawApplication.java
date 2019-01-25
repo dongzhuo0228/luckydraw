b37765bd60d904f45d6fc913e9a55a9775db6aa7
董茁 <dongz001928@yimidida.com>
2019-01-25 17:34:55 +0800

    抽獎程序

---
+++ src/main/java/com/example/luckydraw/LuckydrawApplication.java
@@ -0,0 +1,14 @@
+package com.example.luckydraw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class LuckydrawApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LuckydrawApplication.class, args);
+    }
+}
---
+++ src/main/resources/static/js/member.js
@@ -0,0 +1,13 @@
+var member = [
+    {"name":'<img src="img/3.jpg" alt="13484927420">',"phone":"123456"},
+    {"name":'<img src="img/3.jpg" alt="13484927420">',"phone":"123456"},
+    {"name":'<img src="img/3.jpg" alt="13484927420">',"phone":"123456"},
+    {"name":'<img src="img/3.jpg" alt="13484927420">',"phone":"123456"},
+    {"name":'<img src="img/3.jpg" alt="13484927420">',"phone":"123456"},
+    {"name":'<img src="img/3.jpg" alt="13484927420">',"phone":"123456"},
+    {"name":'<img src="img/3.jpg" alt="13484927420">',"phone":"123456"},
+    {"name":'<img src="img/3.jpg" alt="13484927420">',"phone":"123456"},
+    {"name":'<img src="img/3.jpg" alt="13484927420">',"phone":"123456"},
+    {"name":'<img src="img/3.jpg" alt="13484927420">',"phone":"123456"},
+    {"name":'<img src="img/3.jpg" alt="13484927420">',"phone":"123456"}
+]
